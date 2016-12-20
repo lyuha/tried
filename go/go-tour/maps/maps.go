a428758645836d009c6cb6d84360411f0713f247
Lyuha <lyuha@users.noreply.github.com>
2016-12-21 04:00:31 +0900

    Add Exercise: Maps in A Tour of Go

---
+++ go/go-tour/maps/maps.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"golang.org/x/tour/wc"
+	"strings"
+)
+
+func WordCount(s string) map[string]int {
+	wordCount := make(map[string]int)
+	for _, v := range strings.Fields(s) {
+		_, ok := wordCount[v]
+		if ok == true {
+			wordCount[v] += 1
+		} else {
+			wordCount[v] = 1
+		}
+	}
+	return wordCount
+}
+
+func main() {
+	wc.Test(WordCount)
+}
